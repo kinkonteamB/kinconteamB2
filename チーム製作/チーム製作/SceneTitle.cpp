efee9cf8296ed36352317dbfc816d5a4d2396386
GA1 <GA1@DEV1-19>
2018-11-01 13:42:21 +0900

    sawao

---
+++ チーム製作/チーム製作/SceneTitle.cpp
@@ -0,0 +1,45 @@
+//STLデバック機能をOFFにする
+#define _SECURE_SCL (0)
+#define _HAS_ITERATOR_DEBUGGING (0)
+
+//GameLで使用するヘッダー
+#include "GameL\SceneObjManager.h"
+#include "GameL\DrawFont.h"
+#include "GameL\Audio.h"
+#include"GameL\DrawTexture.h"
+//使用するネームスペース
+using namespace GameL;
+
+//使用するヘッダー
+#include "SceneMain.h"
+#include "GameHead.h"
+#include "SceneTitle.h"
+
+//コンストラクタ
+CSceneTitle::CSceneTitle()
+{
+
+}
+
+//デストラクタ
+CSceneTitle::~CSceneTitle()
+{
+
+}
+
+//ゲームメイン初期化メソッド
+void CSceneTitle::InitScene()
+{
+	//出力させる文字のグラフィックを作成
+	Draw::LoadImage(L"647749.jpg", 3, TEX_SIZE_1600);
+
+	//背景オブジェクト作成
+	CObjTitle* back = new CObjTitle();
+	Objs::InsertObj(back, OBJ_TITLE, 3);
+}
+
+//ゲームメイン実行中メソッド
+void CSceneTitle::Scene()
+{
+
+}
