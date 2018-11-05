b8a1e436bd4ae1c9367e6614ce0db6d569d9c722
GA1 <GA1@DEV1-14>
2018-11-05 14:03:14 +0900

    sawao

---
+++ チーム製作/チーム製作/ObjTitle.cpp
@@ -0,0 +1,61 @@
+//使用するヘッダーファイル
+#include "GameL\DrawFont.h"
+#include "GameL\WinInputs.h"
+#include "GameL\SceneManager.h"
+
+#include "GameHead.h"
+#include "ObjTitle.h"
+
+#include"GameL\DrawTexture.h"
+
+//使用するネームスペース
+using namespace GameL;
+
+//イニシャライズ
+void CObjTitle::Init()
+{
+	m_key_flag = false;
+}
+
+//アクション
+void CObjTitle::Action()
+{
+	//エンターキーを押してシーン:ゲームメインに移行する
+	if (Input::GetVKey(VK_RETURN) == true)
+	{
+		if (m_key_flag == true)
+		{
+			Scene::SetScene(new CSceneMain());
+			m_key_flag = false;
+		}
+	}
+	else
+	{
+		m_key_flag = true;
+	}
+}
+//ドロー
+void CObjTitle::Draw()
+{
+	//描写カラー情報
+	float c[4] = { 1.0f,1.0f,1.0f,1.0f, };
+
+	RECT_F src;//描写元切り取り位置
+	RECT_F dst;//描写先表示位置
+
+			   //切り取り位置の設定
+	src.m_top = 0.0f;
+	src.m_left = 0.0f;
+	src.m_right = 1600.0f;
+	src.m_bottom = 909.0f;
+
+	//表示位置の設定
+	dst.m_top = 0.0f;
+	dst.m_left = 0.0f;
+	dst.m_right = 800.0f;
+	dst.m_bottom = 600.0f;
+
+	//0番目に登録したグラフィックをsrc・dst・ｃの情報を元に描写
+	Draw::Draw(3, &src, &dst, c, 0.0f);
+
+}
--- チーム製作/チーム製作/SceneMain.cpp
+++ チーム製作/チーム製作/SceneMain.cpp
@@ -34,10 +34,10 @@ void CSceneMain::InitScene()
 	//外部グラフィックファイルを読み込み0番に登録（576x384ピクセル）
 	Draw::LoadImage(L"1133010303.png", 0, TEX_SIZE_576);
 
-
 	//主人公オブジェクト作成
 	C0bjHero* obj = new C0bjHero();   //主人公オブジェクト作成
 	Objs::InsertObj(obj, COBJ_HERO, 10);//作った主人公オブジェクトをオブジェクトマネージャーに登録
+
 
 }
 
