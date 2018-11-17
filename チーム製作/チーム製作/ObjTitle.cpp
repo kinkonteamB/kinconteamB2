949be0263958006ce3edca7f96c750af67dfcdcc
RIKUTO <RIKUTO@R1TO-PC>
2018-11-17 15:39:50 +0900

    キー操作　木村

--- チーム製作/チーム製作/ObjTitle.cpp
+++ チーム製作/チーム製作/ObjTitle.cpp
@@ -14,42 +14,32 @@ using namespace GameL;
 //イニシャライズ
 void CObjTitle::Init()
 {
-	m_key_flag = false;
-	//m_mou_x = 0.0f;
-	//m_mou_y = 0.0f;
-	choose = 3;
+	m_key_flag = true;
+	choose = 0;
+	m_time = 10;
 }
 
 //アクション
 void CObjTitle::Action()
 {
-	if (Input::GetVKey(VK_UP) == true && choose < 3)
+	if (Input::GetVKey(VK_UP) == true && choose > 0 && m_time == 0)
 	{
-		if (m_f = true)
-		{
-			++choose;
-			m_f=false;
-		}
+		--choose;
+		m_time = 10;
 	}
-	else
+	if (Input::GetVKey(VK_DOWN) == true && choose < 2 && m_time == 0)
 	{
-		m_f = true;
+		++choose;
+		m_time = 10;
 	}
 
-	if (Input::GetVKey(VK_DOWN) == true && choose > 1)
-	{
-		if (m_f = true)
-		{
-			--choose;
-			m_f = false;
+	if (m_time > 0) {
+		m_time--;
+		if (m_time <= 0) {
+			m_time = 0;
 		}
 	}
-	else
-	{
-		m_f = true;
-	}
-
-	if (choose == 3)
+	if (choose == 0)
 	{
 		if (Input::GetVKey(VK_RETURN) == true)
 		{
@@ -63,7 +53,7 @@ void CObjTitle::Action()
 			m_key_flag = true;
 		}
 	}
-	if (choose == 2)
+	if (choose == 1)
 	{
 		if (Input::GetVKey(VK_RETURN) == true)
 		{
@@ -77,7 +67,7 @@ void CObjTitle::Action()
 			m_key_flag = true;
 		}
 	}
-	if (choose == 1)
+	if (choose == 2)
 	{
 		if (Input::GetVKey(VK_RETURN) == true)
 		{
@@ -85,45 +75,6 @@ void CObjTitle::Action()
 		}
 	}
 }
-	////マウスの位置を取得
-	//m_mou_x = (float)Input::GetPosX();
-	//m_mou_y = (float)Input::GetPosY();
-
-	////マウスの位置とクリックする場所で当たり判定
-	//if (m_mou_x > MOU_START_HIT_X && m_mou_x<MOU_START_HIT_V &&
-	//	m_mou_y>MOU_START_HIT_Y && m_mou_y < MOU_START_HIT_H)
-	//{
-	//	//マウスのボタンが押されたらメインに遷移
-	//	if (m_mou_r == true || m_mou_l == true)
-
-	//	{
-	//		Scene::SetScene(new CSceneMain());
-	//	}
-	//}
-
-	//if (m_mou_x > GAME_RANKING_POS_X -3 && m_mou_x < GAME_RANKING_POS_X +209 &&
-	//	m_mou_y > GAME_RANKING_POS_Y && m_mou_y < GAME_RANKING_POS_Y +40)
-	//{
-	//	if (m_mou_r == true || m_mou_l == true)
-	//	{
-	//		Scene::SetScene(new CSceneRanking());
-	//	}
-	//}
-
-	//if (m_mou_x > GAME_EXIT_POS_X && m_mou_x < GAME_EXIT_POS_X +112 &&
-	//	m_mou_y > GAME_EXIT_POS_Y && m_mou_y < GAME_EXIT_POS_Y +25)
-	//{
-	//	if (m_mou_r == true || m_mou_l == true)
-	//	{
-	//		exit(1);
-	//	}
-	//}
-	////マウスの位置を取得
-	//m_mou_x = (float)Input::GetPosX();
-	//m_mou_y = (float)Input::GetPosY();
-	////マウスのボタンの状態
-	//m_mou_r = Input::GetMouButtonR();
-	//m_mou_l = Input::GetMouButtonL();
 
 //ドロー
 void CObjTitle::Draw()
@@ -154,26 +105,16 @@ void CObjTitle::Draw()
 	//タイトル
 	Font::StrDraw(L"異世界の塔", 148, 120, 100, b);
 
-
-	//if (m_mou_x > 318 && m_mou_x < 464 && m_mou_y < 399 && m_mou_y >349)
-		//Font::StrDraw(L"◇START", GAME_START_POS_X - 55, GAME_START_POS_Y, GAME_START_FONT_SIZE, b);
-
-		Font::StrDraw(L"START", GAME_START_POS_X, GAME_START_POS_Y, GAME_START_FONT_SIZE, b);
-
-	//if (m_mou_x > 287 && m_mou_x < 496 && m_mou_y > 420 && m_mou_y < 460)
-	//	Font::StrDraw(L"◇RANKING", GAME_RANKING_POS_X - 55, GAME_RANKING_POS_Y, GAME_RANKING_FONT_SIZE, b);
-	
-		Font::StrDraw(L"RANKING", GAME_RANKING_POS_X, GAME_RANKING_POS_Y, GAME_RANKING_FONT_SIZE, b);
-
-	//if (m_mou_x > 326 && m_mou_x < 453 && m_mou_y > 487 && m_mou_y < 537)
-		//Font::StrDraw(L"◇EXIT", GAME_EXIT_POS_X - 55, GAME_EXIT_POS_Y, GAME_EXIT_FONT_SIZE, b);
-	
-		Font::StrDraw(L"EXIT", GAME_EXIT_POS_X, GAME_EXIT_POS_Y, GAME_EXIT_FONT_SIZE, b);
-
-		if(choose==3)
-			Font::StrDraw(L"◇", GAME_START_POS_X - 55, GAME_START_POS_Y, GAME_START_FONT_SIZE, b);
-		if(choose==2)
-			Font::StrDraw(L"◇", GAME_RANKING_POS_X - 55, GAME_RANKING_POS_Y, GAME_RANKING_FONT_SIZE, b);
+		if(choose==0)
+			Font::StrDraw(L"◇START", GAME_START_POS_X - 55, GAME_START_POS_Y, GAME_START_FONT_SIZE, b);
+		else
+			Font::StrDraw(L"START", GAME_START_POS_X, GAME_START_POS_Y, GAME_START_FONT_SIZE, b);
 		if(choose==1)
-			Font::StrDraw(L"◇", GAME_EXIT_POS_X - 55, GAME_EXIT_POS_Y, GAME_EXIT_FONT_SIZE, b);
+			Font::StrDraw(L"◇RANKING", GAME_RANKING_POS_X - 55, GAME_RANKING_POS_Y, GAME_RANKING_FONT_SIZE, b);
+		else
+			Font::StrDraw(L"RANKING", GAME_RANKING_POS_X, GAME_RANKING_POS_Y, GAME_RANKING_FONT_SIZE, b);
+		if(choose==2)
+			Font::StrDraw(L"◇EXIT", GAME_EXIT_POS_X - 55, GAME_EXIT_POS_Y, GAME_EXIT_FONT_SIZE, b);
+		else
+			Font::StrDraw(L"EXIT", GAME_EXIT_POS_X, GAME_EXIT_POS_Y, GAME_EXIT_FONT_SIZE, b);
 }
