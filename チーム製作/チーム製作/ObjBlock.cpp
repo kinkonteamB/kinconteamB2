cd4387eb4b99c9072d3c7fbdb51a9c3b573949d8
kinkonteamB <1201819011@hiratagakuen.onmicrosoft.com>
2018-11-19 16:39:28 +0900

    kimura

--- チーム製作/チーム製作/ObjBlock.cpp
+++ チーム製作/チーム製作/ObjBlock.cpp
@@ -62,18 +62,19 @@ void CObjBlock::Draw()
 	RECT_F src;//描写元切り取り位置
 	RECT_F dst;//描写先表示位置
 
-	//切り取り位置の設定
-	src.m_top = 0.0f;
-	src.m_left = 0.0f;
-	src.m_right = 32.0f;
-	src.m_bottom = 32.0f;
-	//m_scroll -= 3.0f;
 
 	for (int i = 0; i < 19; i++)
 	{
 		for (int j = 0; j < 100; j++)
 		{
-			if (m_map[i][j] > 0)
+
+			//切り取り位置の設定
+			src.m_top = 0.0f;
+			src.m_left = 0.0f;
+			src.m_right = 32.0f;
+			src.m_bottom = 32.0f;
+
+			if (m_map[i][j] == 1)
 			{
 				//表示位置の設定
 				dst.m_top = i*32.0f;
@@ -82,14 +83,19 @@ void CObjBlock::Draw()
 				dst.m_bottom = dst.m_top + 32.0f;
 
 				Draw::Draw(2, &src, &dst, c, 0.0f);
+			}
+			else if (m_map[i][j] == 2)
+			{
+				/*src.m_left = 60.0f;
+				src.m_right = 100.0f;*/
 
-		/*		if (m_map[i][j] == 4 || m_map[i][j] == 5)
-				{
-					dst.m_top = i*32.0f;
-					dst.m_left = j*32.0f;
-					dst.m_right = dst.m_left + 32.0f;
-					dst.m_bottom = dst.m_top + 32.0f;
-				}*/
+				//表示位置の設定
+				dst.m_top = i*32.0f;
+				dst.m_left = j*32.0f + m_scroll;
+				dst.m_right = dst.m_left + 32.0f;
+				dst.m_bottom = dst.m_top + 32.0f;
+
+				Draw::Draw(4, &src, &dst, c, 0.0f);
 			}
 		}
 	}
@@ -167,7 +173,7 @@ void CObjBlock::BlockHit(
 							//右
 							*right = true;//オブジェクトの左の部分が衝突している
 							*x = bx + 32.0f + (scroll);//ブロックの位置+主人公の幅
-								*vx = -(*vx)*0.0f;//-VX*反発係数
+							*vx = -(*vx)*0.0f;//-VX*反発係数
 						}
 						if (r > 45 && r < 135)
 						{
@@ -175,6 +181,10 @@ void CObjBlock::BlockHit(
 							*down = true;//主人公の下の部分が衝突している
 							*y = by - 64.0f;//ブロックの位置-主人公の幅
 							*vy = 0.0f;
+							if (m_map[i][j] == 2)
+							{
+								Scene::SetScene(new CSceneOver());
+							}
 						}
 						if (r > 135 && r < 225)
 						{
